7e4862e0f6aeae8bf24906143011698b7c2d2972
chochojj <104323906+chochojj@users.noreply.github.com>
2023-02-08 00:02:46 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -4,3 +4,10 @@ function solution(angle) {
     else if(90 < angle && angle < 180) return 3
     else if(angle === 180 ) return 4
 }
+
+
+
+// 찢었다...
+// function solution(angle) {
+//     return [0, 90, 91, 180].filter(x => angle>=x).length;
+// }
