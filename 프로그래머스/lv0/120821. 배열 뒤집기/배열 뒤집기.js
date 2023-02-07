f6557d0e7d2c859304aad17688b80324dfb7d394
chochojj <104323906+chochojj@users.noreply.github.com>
2023-02-08 00:13:44 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,3 +1,14 @@
 function solution(num_list) {
     return num_list.reverse()
 }
+
+
+// 알고리즘을 짜는 능력을 보는 문제라하면 reverse()를 쓰지않고 풀어야하는걸까?
+// function solution(num_list) {
+//     var answer = [];
+//     var j = num_list.length
+//     for(var i = 1; i <= j; i++){
+//         answer.push(num_list[j-i])
+//     }
+//     return answer;
+// }
