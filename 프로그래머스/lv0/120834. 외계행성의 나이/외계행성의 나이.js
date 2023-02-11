baffac362b979a6f5573ccd5c6a47eb767405b9d
chochojj <104323906+chochojj@users.noreply.github.com>
2023-02-11 23:14:32 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -10,3 +10,12 @@ function solution(age) {
      
      return trans
 }
+
+//문자열 인덱스를 바로 조회해서 빼냄
+// function solution(age) {
+//   return age
+//     .toString()
+//     .split("")
+//     .map((v) => "abcdefghij"[v])
+//     .join("");
+// }
