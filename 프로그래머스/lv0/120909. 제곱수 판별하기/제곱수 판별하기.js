32a0d9e7a0583459fc23e1a869d9d421036c7adb
chochojj <104323906+chochojj@users.noreply.github.com>
2023-02-13 21:51:18 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -2,3 +2,13 @@ function solution(n) {
     if(Math.sqrt(n)%1 !== 0 )return 2
     return 1
 }
+
+// 내가 하고싶었던 방법
+// function solution(n) {
+//     for (let i = 0; i < n/2; i++) {
+//         if (i*i == n) {
+//             return 1;
+//         }
+//     }
+//     return 2;
+// }
