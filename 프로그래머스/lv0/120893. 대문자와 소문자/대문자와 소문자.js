99c80ecba818e5f2c260304d843285f132edbe08
chochojj <104323906+chochojj@users.noreply.github.com>
2023-03-03 14:43:54 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -5,3 +5,11 @@ function solution(my_string) {
     }).join("")
        
 }
+
+
+//삼항연산자 사용해도 편하겠다
+// function solution(my_string) {
+//     var answer = '';
+//     for (let c of my_string) answer += c === c.toLowerCase() ? c.toUpperCase() : c.toLowerCase();
+//     return answer;
+// }
