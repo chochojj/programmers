c43b649638e0c2cfb05abed107cb6a21e9bc6215
chochojj <104323906+chochojj@users.noreply.github.com>
2023-03-04 22:59:06 +0900

    [level 0] Title: 팩토리얼, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -16,3 +16,6 @@ function fac(num) {
     if(num > 1) return num*fac(num-1)
     return num
 }
+
+//숫자를 먼저 계산해서 구하는 방법도 있음 신박
+// let solution=n=>10-[0,1,2,6,24,120,720,5040,40320,362880,3628800,n].sort((a,b)=>a-b).reverse().indexOf(n)
