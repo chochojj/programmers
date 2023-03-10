e3b636a10fd03f34da0fae3fb0875c711fdf44dd
chochojj <104323906+chochojj@users.noreply.github.com>
2023-03-10 18:11:58 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -3,3 +3,7 @@ function solution(numbers) {
     let max = [numbers[0]*numbers[1], numbers[numbers.length-1]*numbers[numbers.length-2]].sort((a,b)=> b -a)
     return max[0]
 }
+
+
+//Math.max()매소드가 기억이 안났음
+//기억해두자궁
