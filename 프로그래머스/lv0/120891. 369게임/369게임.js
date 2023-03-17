d1abf9ccf601df28ab7fe6713b203d46f2138f5d
chochojj <104323906+chochojj@users.noreply.github.com>
2023-03-17 14:45:47 +0900

    [level 0] Title: 369게임, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120891. 369게임/369게임.js
+++ 프로그래머스/lv0/120891. 369게임/369게임.js
@@ -1,11 +1,15 @@
+function solution(order) {
+    return order.toString().split('').filter(n => ['3', '6', '9'].includes(n)).length;
+}
+
 // function solution(order) {
 //     var answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
 //     return answer;
 // }
 
-function solution(order) {
-    const mySet = new Set([3,6,9]);
-    return String(order).split('')
-                        .filter(num => mySet.has(Number(num)))
-                        .length;
-}
+// function solution(order) {
+//     const mySet = new Set([3,6,9]);
+//     return String(order).split('')
+//                         .filter(num => mySet.has(Number(num)))
+//                         .length;
+// }
