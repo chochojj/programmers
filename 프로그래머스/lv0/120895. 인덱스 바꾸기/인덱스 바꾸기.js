ae8772c78293ec895dc1633d758443c810dcb24a
chochojj <104323906+chochojj@users.noreply.github.com>
2023-03-17 14:55:00 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -3,7 +3,6 @@ function solution(my_string, num1, num2) {
     let n1 = arr[num1]
     let n2 = arr[num2]
     for(let i = 0; i < my_string.length; i++){
-        
         if(i===num1){
             arr[num1] = n2
         }
@@ -11,5 +10,8 @@ function solution(my_string, num1, num2) {
             arr[num2] = n1
         }
     }
+    //위의 포문의 역할을
+    // [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+    // 로 바꿔 써도 같은 기능을 함
     return arr.join('')
 }
