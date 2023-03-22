6fb35b37f836190b96f58cf5f09fad3be0d1da0c
chochojj <104323906+chochojj@users.noreply.github.com>
2023-03-22 17:45:54 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.js
@@ -3,3 +3,7 @@ function solution(num, k) {
     if(n.includes(k.toString())) return n.indexOf(k.toString())+1
     else return -1
 }
+
+// function solution(num, k) {
+//     return num.toString().split("").map((el) => Number(el)).indexOf(k) + 1 || -1
+// }
