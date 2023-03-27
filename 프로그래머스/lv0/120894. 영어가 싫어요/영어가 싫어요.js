8512f9c16a79036a29c16e9725ff471f7ab1796d
chochojj <104323906+chochojj@users.noreply.github.com>
2023-03-27 21:03:26 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -6,3 +6,12 @@ function solution(numbers) {
     });
     return Number(numbers);
 }
+
+
+// function solution(numbers) {
+//     const number = ["zero","one","two","three", "four", "five", "six", "seven", "eight", "nine"]
+//     for(let i = 0 ; i<number.length; i++){
+//         numbers = numbers.split(number[i]).join(i)
+//     }
+//     return +numbers
+// }
