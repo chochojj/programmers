0fcb10a4de546b1dcd749ed5edde5e27a17dee17
chochojj <104323906+chochojj@users.noreply.github.com>
2023-04-30 12:59:02 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,3 +1,9 @@
 function solution(my_string) {
     return my_string.toLowerCase().split("").sort().join("")
 }
+
+
+//구조분해할당 사용하기
+// function solution(s) {
+//     return [...s.toLowerCase()].sort().join('')
+// }
