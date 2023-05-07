f29dfc1d661f480a65f49294120419812f9a505b
chochojj <104323906+chochojj@users.noreply.github.com>
2023-05-07 23:07:29 +0900

    [level 0] Title: k의 개수, Time: 10.19 ms, Memory: 39.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -1,8 +1,21 @@
 function solution(i, j, k) {
-   let arr = []
+    //배열이 아니라 문자열로 초기 설정을 하면 .join("")로 묶어줄 필요가 없어서 
+    //단계가 하나 감소함!
+   let str = '';
    for(let n  = i; n <= j; n++){
-       arr.push(n)
+        str += n
    }
-    
-    return arr.join("").split("").filter(el => el === k.toString()).length
+    return str.split("").filter(el => el === k.toString()).length
+
 }
+
+
+
+// function solution(i, j, k) {
+//    let arr = []
+//    for(let n  = i; n <= j; n++){
+//        arr.push(n)
+//    }
+    
+//     return arr.join("").split("").filter(el => el === k.toString()).length
+// }
